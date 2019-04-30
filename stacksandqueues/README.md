5818c9dc11a1cf0c9d977c45441798694ebe7e51
David <dodwei.dev@gmail.com>
2019-04-30 14:59:52 +0800

    [StackWithMax]1st attempt

--- stacksandqueues/README.md
+++ stacksandqueues/README.md
@@ -1,6 +1,6 @@
 # Chapter 9: Stacks and Queues
 
-- [ ] 9.1 StackWithMax
+- [x] 9.1 StackWithMax ✅1
 - [ ] 9.2 EvaluateRPNExpressions
 - [ ] 9.3 IsStringWellFormed
 - [ ] 9.4 NormalizedPathnames
--- stacksandqueues/src/main/java/StackWithMax.java
+++ stacksandqueues/src/main/java/StackWithMax.java
@@ -1,22 +1,32 @@
 public class StackWithMax {
+    private Node first;
 
-    /*
-    9.1
-    */
-
-    public StackWithMax() {
-
+    private class Node {
+        Integer data;
+        Node next;
     }
 
     public Integer max() {
-        return 0;
+        Integer max = 0;
+        Node current = first;
+        while (current != null) {
+            max = Math.max(max, current.data);
+            current = current.next;
+        }
+        return max;
     }
 
     public Integer pop() {
-        return 0;
+        Node item = first;
+        first = first.next;
+
+        return item.data;
     }
 
     public void push(Integer x) {
-
+        Node oldFirst = first;
+        first = new Node();
+        first.data = x;
+        first.next = oldFirst;
     }
 }

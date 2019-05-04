4bd2dfcdf9a3686f3a970aaa6cca1fe5cfa2854e
David <dodwei.dev@gmail.com>
2019-05-04 13:47:21 +0800

    [MergeSortedFiles]1st attempt

--- heaps/src/main/java/MergeSortedFiles.java
+++ heaps/src/main/java/MergeSortedFiles.java
@@ -1,14 +1,27 @@
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.PriorityQueue;
 
 public class MergeSortedFiles {
 
     /*
-    11.1
-    */
+     * 11.1
+     */
 
-    public static List<Integer> mergeSorted(List<List<Integer>> sortedArrays) {
+    public static List<Integer> mergeSorted1(List<List<Integer>> sortedArrays) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
 
-        return Collections.emptyList();
+        for (List<Integer> array : sortedArrays) {
+            for (Integer integer : array) {
+                heap.add(integer);
+            }
+        }
+
+        List<Integer> result = new ArrayList<>();
+        while (!heap.isEmpty()) {
+            result.add(heap.remove());
+        }
+
+        return result;
     }
 }
--- heaps/src/test/java/MergeSortedFilesTest.java
+++ heaps/src/test/java/MergeSortedFilesTest.java
@@ -12,29 +12,22 @@ public class MergeSortedFilesTest {
 
     @Test
     public void mergeSorted1() throws Exception {
-        expected = Arrays.asList(1,2,3,4,5,6,7,8,9);
-        input = Arrays.asList(
-                Arrays.asList(1,4,7),
-                Arrays.asList(2,5,8),
-                Arrays.asList(3,6,9)
-        );
+        expected = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9);
+        input = Arrays.asList(Arrays.asList(1, 4, 7), Arrays.asList(2, 5, 8), Arrays.asList(3, 6, 9));
 
         test(expected, input);
     }
 
     @Test
     public void mergeSorted2() throws Exception {
-        expected = Arrays.asList(0,0,3,5,6,6,7,28);
-        input = Arrays.asList(
-                Arrays.asList(3,5,7),
-                Arrays.asList(0,6),
-                Arrays.asList(0,6,28)
-        );
+        expected = Arrays.asList(0, 0, 3, 5, 6, 6, 7, 28);
+        input = Arrays.asList(Arrays.asList(3, 5, 7), Arrays.asList(0, 6), Arrays.asList(0, 6, 28));
 
         test(expected, input);
     }
 
     private void test(List<Integer> expected, List<List<Integer>> input) {
-        assertEquals(expected, MergeSortedFiles.mergeSorted(input));
+        // 1st attempt
+        assertEquals(expected, MergeSortedFiles.mergeSorted1(input));
     }
 }

6ebb29c71fe5ee0ebfd0acd8ef2f4699934629d4
David <dodwei.dev@gmail.com>
2019-08-07 16:15:40 +0800

    [ComputeBinaryTreeNodes]1st attempt

--- stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
+++ stacksandqueues/src/main/java/ComputeBinaryTreeNodes.java
@@ -1,14 +1,41 @@
-import java.util.Collections;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class ComputeBinaryTreeNodes {
 
     /*
-    9.7
-    */
+     * 9.7
+     */
 
     public static List<List<Integer>> binaryTreeDepthOrder(BinaryTree<Integer> tree) {
 
-        return Collections.emptyList();
+        Queue<BinaryTree<Integer>> currentLevelNodeQueue = new LinkedList<>();
+        currentLevelNodeQueue.add(tree);
+        List<List<Integer>> result = new ArrayList<>();
+
+        while (!currentLevelNodeQueue.isEmpty()) {
+            Queue<BinaryTree<Integer>> nextLevelNodQueue = new LinkedList<>();
+            List<Integer> currentLevel = new ArrayList<>();
+
+            while (!currentLevelNodeQueue.isEmpty()) {
+                BinaryTree<Integer> node = currentLevelNodeQueue.poll();
+
+                if (node != null) {
+                    currentLevel.add(node.data);
+                    nextLevelNodQueue.add(node.left);
+                    nextLevelNodQueue.add(node.right);
+                }
+            }
+
+            if (!currentLevel.isEmpty()) {
+                result.add(currentLevel);
+            }
+            currentLevelNodeQueue = nextLevelNodQueue;
+        }
+
+        return result;
     }
 }

5f4a6aefb01fa69620a9e8c1f75a04e64fb9738e
David <dodwei.dev@gmail.com>
2019-09-06 10:20:12 +0800

    [FirstGreaterThan]1st attempt

--- binarysearchtrees/README.md
+++ binarysearchtrees/README.md
@@ -1,7 +1,7 @@
 # Chapter 15: Binary Search Trees
 
-- [x] 15.1 IsBST ✅1
-- [ ] 15.2 FirstGreaterThan
+- [x] 15.1 IsBST ✅
+- [x] 15.2 FirstGreaterThan ✅
 - [ ] 15.3 FindKLargest
 - [ ] 15.4 ComputeLCA
 - [ ] 15.5 ReconstructBST
--- binarysearchtrees/src/main/java/FirstGreaterThan.java
+++ binarysearchtrees/src/main/java/FirstGreaterThan.java
@@ -1,11 +1,20 @@
 public class FirstGreaterThan {
 
     /*
-    15.2
-    */
+     * 15.2
+     */
 
     public static BinaryTree<Integer> find(BinaryTree<Integer> tree, Integer k) {
+        BinaryTree<Integer> subtree = tree, firstSoFar = null;
 
-        return new BinaryTree<>(0);
+        while (subtree != null) {
+            if (subtree.data > k) {
+                firstSoFar = subtree;
+                subtree = subtree.left;
+            } else {
+                subtree = subtree.right;
+            }
+        }
+        return firstSoFar;
     }
 }
